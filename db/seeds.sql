bbec652fb200e0281baad22d06020edc9585d5db
Carter Supple <Cartersupple123@gmail.com>
2022-02-18 12:48:23 -0800

    added seeds to database

---
+++ db/seeds.sql
@@ -0,0 +1,26 @@
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES
+('John', 'Doe', 1, 1),
+('Sarah', 'Whips', 1, 1),
+('Tim', 'Saab', 2, 2),
+('Jim', 'Proob', 2, 2),
+('Jeff', 'Probes', 3, 3),
+('Timmy', 'Wang', 3, 3),
+('Jimmy', 'Hendrix', 4, 4),
+('Buzz', 'Aldrine', 4, 4),
+('Jeff', 'Bezos', 5, 5),
+('Elongated', 'Musk', 5, 5),
+INSERT INTO roles (title, salary, department_id)
+VALUES
+('CEO',1000000000 , 1),
+('Manager', 1000000, 2),
+('Clerk', 100000, 3),
+('Commoner', 1000, 4),
+('Peasant', 100, 5),
+  INSERT INTO department (id, name)
+VALUES
+(1, 'Armored Division'),
+(2, 'Air Force'),
+(3, 'Cavalry'),
+(4, 'Infantry'),
+(5, 'Cannon Fauder'),
